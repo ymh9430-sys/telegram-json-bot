1b728d2a87279ff4479350bb6e5873f05d3be5c0
ymh9430-sys <ymh9430@gmail.com>
2026-03-02 18:45:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import re
 import os
 from bs4 import BeautifulSoup
 
-TOKEN = "PUT_YOUR_TOKEN_HERE"
+TOKEN = os.getenv("TOKEN")"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
