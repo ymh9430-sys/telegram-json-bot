3cee928eb21b2fe098afd72d3a4450a6cd0c998d
ymh9430-sys <ymh9430@gmail.com>
2026-03-05 23:11:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,35 +4,49 @@ import re
 from playwright.sync_api import sync_playwright
 
 TOKEN = os.getenv("TOKEN")
+
 bot = telebot.TeleBot(TOKEN)
 
 def get_lyrics_from_ytmusic(url):
     with sync_playwright() as p:
+
         browser = p.chromium.launch(headless=True)
+
         page = browser.new_page()
 
-        page.goto(url)
+        page.goto(url, timeout=60000)
 
-        # نستنى الصفحة تحمل
         page.wait_for_timeout(8000)
 
-        # نحاول نجيب النص من الصفحة
-        content = page.content()
+        html = page.content()
 
         browser.close()
 
-        return content
+        return html
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message,"ابعت:\n/yt رابط الأغنية من YouTube Music")
+
+    bot.reply_to(message,
+"""
+اهلا 👋
+
+الاوامر:
+
+/yt رابط الاغنية من YouTube Music
+""")
+
+
 
 @bot.message_handler(commands=['yt'])
 def yt(message):
+
     try:
+
         url = message.text.split(" ",1)[1]
 
-        bot.reply_to(message,"جاري جلب الصفحة...")
+        bot.reply_to(message,"جاري فتح الصفحة...")
 
         html = get_lyrics_from_ytmusic(url)
 
@@ -43,4 +57,8 @@ def yt(message):
         bot.send_message(message.chat.id,result)
 
     except Exception as e:
-        bot.send_message(message.chat.id, f"Error:\n{e}")
+
+        bot.send_message(message.chat.id,f"Error:\n{e}")
+
+
+bot.infinity_polling()
