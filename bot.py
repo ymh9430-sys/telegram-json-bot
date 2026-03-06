0e5f11b98df80bed46d65eb07e9b451e99a99127
ymh9430-sys <ymh9430@gmail.com>
2026-03-06 03:26:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = "PUT_YOUR_BOT_TOKEN_HERE"
+TOKEN = "8509336206:AAHnNtM7e9CUeJYeUEZLJT8ZJMlJIeF8hYk"
 
 
 def extract_video_id(url):
