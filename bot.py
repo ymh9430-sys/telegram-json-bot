6a78847b099dd2035819a55baddc7682e356abec
ymh9430-sys <ymh9430@gmail.com>
2026-03-06 17:22:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,20 +16,33 @@ def get_lyrics(title, artist, duration=0):
         "d": duration
     }
 
-    r = requests.get(url, params=params)
+    try:
+        r = requests.get(url, params=params, timeout=10)
 
-    if r.status_code == 200:
-        data = r.json()
-        return data.get("ttml")
+        if r.status_code == 200:
+            data = r.json()
+            return data.get("ttml")
+
+    except:
+        return None
+
     return None
 
 
 @bot.message_handler(commands=['yt'])
 def handle(message):
     try:
+        if " " not in message.text:
+            bot.reply_to(message,"ارسل الامر هكذا:\n/yt رابط الاغنية")
+            return
+
         link = message.text.split(" ",1)[1]
 
-        video_id = link.split("v=")[1]
+        if "v=" not in link:
+            bot.reply_to(message,"الرابط غير صحيح")
+            return
+
+        video_id = link.split("v=")[1].split("&")[0]
 
         info = yt.get_song(video_id)
 
@@ -42,11 +55,13 @@ def handle(message):
         lyrics = get_lyrics(title,artist,duration)
 
         if lyrics:
-            bot.send_message(message.chat.id,lyrics[:4000])
+            bot.send_message(message.chat.id, lyrics[:4000])
         else:
             bot.send_message(message.chat.id,"❌ لم يتم العثور على كلمات")
 
     except Exception as e:
-        bot.send_message(message.chat.id,str(e))
+        bot.send_message(message.chat.id,f"خطأ:\n{str(e)}")
 
+
+print("Bot Started...")
 bot.infinity_polling()
